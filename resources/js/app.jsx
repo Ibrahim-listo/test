eafc9bd92c8ff1156ec33fed3800e74df47c2bf9
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:23 +0200

    improve

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,22 +1,5 @@
 import "./bootstrap";
 import "../css/app.css";
-
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
-
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolvePageComponent: (name) =>
-        resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob("./Pages/**/*.jsx")),
-    setup({ element, App, props }) {
-        const root = createRoot(element);
-
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: "#4B5563",
-    },
-});
