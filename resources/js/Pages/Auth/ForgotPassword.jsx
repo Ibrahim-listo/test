82a696d767d81a7f946c01a42507d9a4f28a3c0f
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:37 +0200

    improve

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -15,31 +15,13 @@ export default function ForgotPassword({ status }) {
         };
     }, []);
 
-    const submit = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            post(route('password.email'));
-        }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        post(route('password.email'));
     };
 
     return (
         <GuestLayout>
             <Head title="Forgot Password" />
 
-            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                Forgot your password? No problem. Just let us know your email address and we will email you a password
-                reset link that will allow you to choose a new one.
-            </div>
-
-            {status && <div className="mb-4 font-medium text-sm text-green-600 dark:text-green-400">{status}</div>}
-
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                    onKey
+            <div className="mb-4 text-sm text-gray-600 dark:text-gray-40
