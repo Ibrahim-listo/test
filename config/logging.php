6468382d0ee3448f4d75e8a60d41d5f2706ba110
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:50 +0200

    improve

--- config/logging.php
+++ config/logging.php
@@ -6,37 +6,63 @@ use Monolog\Handler\SyslogUdpHandler;
 use Monolog\Processor\PsrLogMessageProcessor;
 
 return [
-
     /*
-    |--------------------------------------------------------------------------
-    | Default Log Channel
-    |--------------------------------------------------------------------------
-    |
-    | This option defines the default log channel that is utilized to write
-    | messages to your logs. The value provided here should match one of
-    | the channels present in the list of "channels" configured below.
-    |
-    */
-
+     |--------------------------------------------------------------------------
+     | Default Log Channel
+     |--------------------------------------------------------------------------
+     |
+     | This option defines the default log channel that is utilized to write
+     | messages to your logs. The value provided here should match one of
+     | the channels present in the list of "channels" configured below.
+     |
+     */
     'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Deprecations Log Channel
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the log channel that should be used to log warnings
-    | regarding deprecated PHP and library features. This allows you to get
-    | your application ready for upcoming major versions of dependencies.
-    |
-    */
-
+     |--------------------------------------------------------------------------
+     | Deprecations Log Channel
+     |--------------------------------------------------------------------------
+     |
+     | This option controls the log channel that should be used to log warnings
+     | regarding deprecated PHP and library features. This allows you to get
+     | your application ready for upcoming major versions of dependencies.
+     |
+     */
     'deprecations' => [
         'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
         'trace' => env('LOG_DEPRECATIONS_TRACE', false),
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Log Channels
-    |------------------------------------------------
+     |--------------------------------------------------------------------------
+     | Log Channels
+     |--------------------------------------------------------------------------
+     |
+     | Here you may configure the log channels for your application. Out of
+     | the box, Laravel uses the Monolog PHP logging library. This gives
+     | you a variety of powerful log handlers / formatters to utilize.
+     |
+     | Available Drivers: "single", "daily", "slack", "syslog",
+     |                    "errorlog", "monolog", "custom", "stack"
+     |
+     */
+
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single', 'syslog'],
+            'ignore_exceptions' => false,
+        ],
+
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
+        ],
+
+        'syslog' => [
+            'driver' => 'syslog',
+            'level' => 'warning',
+        ],
+
+
