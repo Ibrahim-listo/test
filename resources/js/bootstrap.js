50f7a090a55cd78ba51d4f34511034967dc441ef
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:23 +0200

    comment

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,9 @@
+// Import the Axios library, which provides a simple way to make HTTP requests in JavaScript.
 import axios from 'axios';
 
+// Set the default headers for all Axios requests to include 'X-Requested-With' with a value of 'XMLHttpRequest'.
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// If you still want to add axios to the global window object, you can do it like this:
+// If the user has requested that Axios be added to the global window object (for use in the browser), do so here.
+// The 'any' type is used to bypass TypeScript's strict type checking.
 (window as any).axios = axios;
