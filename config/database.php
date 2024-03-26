1dbe434afcaf9e28b03f678143f3315de517d849
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:44 +0200

    comment

--- config/database.php
+++ config/database.php
@@ -2,35 +2,16 @@
 
 use Illuminate\Support\Str;
 
+// Return an array containing the database configuration for the Laravel application.
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
-    */
-
+    // Specify the default database connection to be used by Laravel.
     'default' => env('DB_CONNECTION', 'sqlite'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported by Laravel. You're free to add / remove connections.
-    |
-    */
-
+    // Define all of the database connections for the application.
     'connections' => [
 
+        // SQLite database configuration.
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
@@ -39,6 +20,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        // MySQL database configuration.
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
@@ -59,6 +41,7 @@ return [
             ]) : [],
         ],
 
+        // MariaDB database configuration.
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
@@ -79,6 +62,7 @@ return [
             ]) : [],
         ],
 
+        // PostgreSQL database configuration.
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
@@ -94,6 +78,7 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        // SQL Server database configuration.
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
@@ -111,42 +96,25 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
-
+    // Configuration for the migration repository table.
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
-
+    // Configuration for Redis databases.
     'redis' => [
 
+        // The Redis client to be used.
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
+        // Options for the Redis client.
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
+        // Default Redis database configuration.
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -156,15 +124,7 @@ return [
             'database' => env('REDIS_DB', '0'),
         ],
 
+        // Redis cache database configuration.
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-        ],
-
-    ],
-
-];
+            'host' => env
