ba5bdde4da9fbbb2b7812c27d20c71c6a5b47afb
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:11 +0200

    comment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,22 +2,4 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions as ConfigurationExceptions;
-use Illuminate\Foundation\Configuration\Middleware as ConfigurationMiddleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use App\Http\Middleware\HandleInertiaRequests;
-
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (ConfigurationMiddleware $middleware) {
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ]);
-    })
-    ->withExceptions(function (ConfigurationExceptions $exceptions) {
-        //
-    })->create();
+use Illuminate\Foundation\Configuration
