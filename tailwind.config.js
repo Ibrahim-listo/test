ca1504c215868acdb5ce321d8dc1f91f4810aa69
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:55 +0200

    comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,37 @@
+// Import the default theme from Tailwind CSS
 import defaultTheme from 'tailwindcss/defaultTheme';
+
+// Import the forms module from Tailwind CSS
 import forms from '@tailwindcss/forms';
 
+// The main exported object containing the configuration
 export default {
+  // Enable dark mode based on a class, not a media query
   darkMode: 'class',
-  // Use a glob pattern to match all relevant files
-  content: ['./{vendor/laravel,storage/framework/views,resources/views}/**/*.{blade,php,js,jsx}', './resources/js/**/*.{js,jsx}', './node_modules/flowbite/**/*.js'],
 
+  // An array of glob patterns matching the relevant files
+  content: [
+    './{vendor/laravel,storage/framework/views,resources/views}/**/*.{blade,php,js,jsx}',
+    './resources/js/**/*.{js,jsx}',
+    './node_modules/flowbite/**/*.js'
+  ],
+
+  // Theme configuration
   theme: {
     extend: {
+      // Extend the font family with a custom font
       fontFamily: {
         sans: ['Figtree', ...defaultTheme.fontFamily.sans],
       },
     },
   },
 
+  // Plugins configuration
   plugins: [
+    // Import and configure the forms module
     forms,
+    // Import and configure the Flowbite plugin
     require('flowbite/plugin')
   ],
-}
+};
+
