431b6c73b1d2d83eb589a3554bdccfd5ceb3e78c
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:31 +0200

    improve

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    // Specify the table name
+    protected $table = 'tasks';
+
+    // Specify the timestamps configuration
+    public $timestamps = true;
+
+    // If you want to disable created_at and updated_at columns
+    // public $timestamps = false;
+
+    // If you want to customize created_at and updated_at columns
+    // const CREATED_AT = 'created_date';
+    // const UPDATED_AT = 'updated_date';
 }
