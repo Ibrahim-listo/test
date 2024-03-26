ad4a8e092409a5cb9a58d4bf9fad5cc3fbf38126
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:49 +0200

    comment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,11 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * TestCase class
+ *
+ * This class serves as the base class for all of your test cases. It provides some useful methods for testing your application, such as visiting web pages, making HTTP requests, and interacting with the database.
+ */
 abstract class TestCase extends BaseTestCase
 {
     //
