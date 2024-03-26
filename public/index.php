a684496059c05331f6361b9caf5acaa7cfad6dda
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:44 +0200

    improve

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php') or file_exists($maintenance = __DIR__.'/../storage/framework/down')) {
     require $maintenance;
     http_response_code(Response::HTTP_SERVICE_UNAVAILABLE);
     echo 'The application is currently in maintenance mode. Please try again later.';
@@ -15,7 +15,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 $app = require_once __DIR__.'/../bootstrap/app.php';
@@ -24,4 +24,8 @@ $response = $app->handleRequest(Request::capture());
 
 // Send the response and end the request
 $response->send();
+
+// Optional: Uncomment the line below to dump the response for debugging purposes
+// dump($response);
+
 $app->terminate();
