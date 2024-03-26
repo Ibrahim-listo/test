ac22fbd00db62bbf88930f39c1a4f88a3c2e75fe
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:53 +0200

    improve

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,23 +1,39 @@
+import PropTypes from 'prop-types';
 import { forwardRef, useEffect, useRef } from 'react';
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
-    const input = ref ? ref : useRef();
+const TextInput = forwardRef(
+  (
+    {
+      type = 'text',
+      className,
+      isFocused = false,
+      name,
+      required,
+      ...props
+    },
+    ref
+  ) => {
+    const inputRef = ref && typeof ref === 'object' ? ref : useRef();
 
     useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
-        }
-    }, []);
+      if (isFocused) {
+        inputRef.current.focus();
+      }
+    }, [isFocused]);
 
     return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
-                className
-            }
-            ref={input}
-        />
+      <input
+        {...props}
+        type={type}
+        className={`border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ${className}`}
+        name={name}
+        ref={inputRef}
+        required={required}
+        autoComplete="on"
+      />
     );
-});
+  }
+);
+
+TextInput.propTypes = {
+  type: PropTypes.oneOf(['text', '
