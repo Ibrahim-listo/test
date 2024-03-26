05a904d9a538374839b86aadc330621c1bd7458f
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:48 +0200

    improve

--- database/migrations/2024_03_25_180140_create_projects_table.php
+++ database/migrations/2024_03_25_180140_create_projects_table.php
@@ -11,15 +11,33 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Create the 'projects' table
         Schema::create('projects', function (Blueprint $table) {
+            // Define the 'id' column as the primary key
             $table->id();
+
+            // Define the 'name' column as a string
             $table->string('name');
+
+            // Define the 'description' column as a long text, allowing null values
             $table->longText('description')->nullable();
+
+            // Define the 'due_date' column as a timestamp, allowing null values
             $table->timestamp('due_date')->nullable();
+
+            // Define the 'status' column as a string
             $table->string('status');
+
+            // Define the 'image_path' column as a string, allowing null values
             $table->string('image_path')->nullable();
+
+            // Define the 'created_by' column as a foreign key, referencing the 'id' column of the 'users' table
             $table->foreignId('created_by')->constrained('users');
+
+            // Define the 'updated_by' column as a foreign key, referencing the 'id' column of the 'users' table
             $table->foreignId('updated_by')->constrained('users');
+
+            // Define the 'created_at' and 'updated_at' columns as timestamps
             $table->timestamps();
         });
     }
@@ -29,6 +47,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        // Drop the 'projects' table if it exists
         Schema::dropIfExists('projects');
     }
 };
