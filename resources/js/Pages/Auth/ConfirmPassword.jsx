1be8edf8620e08105e751f6d7e76a4c8b0a24e23
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:21 +0200

    improve

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -35,4 +35,28 @@ export default function ConfirmPassword() {
         <GuestLayout>
             <Head title="Confirm Password" />
 
-            <div className="mb-4 text-sm text-gray-6
+            <div className="mb-4 text-sm text-gray-600">
+                This is a secure area of the application. Please confirm your password before continuing.
+            </div>
+
+            <form onSubmit={submit} onKeyUp={handleKeyUp}>
+                <div>
+                    <InputLabel htmlFor="password" value="Password" />
+
+                    <TextInput
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        className="mt-1 block w-full"
+                        autoComplete="current-password"
+                        isFocused={true}
+                        handleChange={(e) => setData('password', e.target.value)}
+                    />
+
+                    <InputError message={errors.password} className="mt-2" />
+                </div>
+
+                <div className="flex items-center justify-end mt-4">
+                    <PrimaryButton className="ml-4" processing={processing}>
+                        Conf
