c5f6ad2a94e31dc8d173e756f7037afb9d6b484b
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:19 +0200

    improve

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,14 +23,28 @@
             border-color: #e5e7eb;
         }
 
-        ::after,
-        ::before {
-            --tw-content: '';
+        /* Remove default padding and margin from all elements */
+        body, h1, h2, h3, h4, h5, h6, p, ul, ol, dl, blockquote, form, fieldset, legend, textarea, input, button, select, table, th, td, figure, figcaption, footer, header, main, aside {
+            margin: 0;
+            padding: 0;
         }
 
-        body {
-            margin: 0;
-            line-height: inherit;
+        /* Normalize font styles */
+        body, button, input, select, textarea {
+            font-family: Figtree, ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+            font-size: 1rem;
+            font-weight: normal;
+            line-height: 1.5;
+            color: inherit;
+        }
+
+        a {
+            color: inherit;
+            text-decoration: inherit;
+        }
+
+        strong, b {
+            font-weight: bold;
         }
 
         hr {
@@ -39,202 +53,16 @@
             border-top-width: 1px;
         }
 
-        abbr:where([title]) {
-            -webkit-text-decoration: underline dotted;
+        abbr[title] {
             text-decoration: underline dotted;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-size: inherit;
-            font-weight: inherit;
-        }
-
-        a {
-            color: inherit;
-            text-decoration: inherit;
-        }
-
-        b,
-        strong {
-            font-weight: bolder;
-        }
-
-        code,
-        kbd,
-        pre,
-        samp {
-            font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
-            font-feature-settings: normal;
-            font-variation-settings: normal;
-            font-size: 1em;
-        }
-
-        small {
-            font-size: 80%;
-        }
-
-        sub,
-        sup {
-            font-size: 75%;
-            line-height: 0;
-            position: relative;
-            vertical-align: baseline;
-        }
-
-        sub {
-            bottom: -.25em;
-        }
-
-        sup {
-            top: -.5em;
-        }
-
-        table {
-            text-indent: 0;
-            border-color: inherit;
-            border-collapse: collapse;
-        }
-
-        button,
-        input,
-        optgroup,
-        select,
-        textarea {
-            font-family: inherit;
-            font-feature-settings: inherit;
-            font-variation-settings: inherit;
-            font-size: 100%;
-            font-weight: inherit;
-            line-height: inherit;
-            color: inherit;
-            margin: 0;
-            padding: 0;
-        }
-
-        button,
-        select {
-            text-transform: none;
-        }
-
-        [type=button],
-        [type=reset],
-        [type=submit],
-        button {
-            -webkit-appearance: button;
-            background-color: transparent;
-            background-image: none;
-        }
-
-        ::-moz-focusring {
-            outline: auto;
-        }
-
-        ::-moz-ui-invalid {
-            box-shadow: none;
-        }
-
-        progress {
-            vertical-align: baseline;
-        }
-
-        ::-webkit-inner-spin-button,
-        ::-webkit-outer-spin-button {
-            height: auto;
-        }
-
-        [type=search] {
-            -webkit-appearance: textfield;
-            outline-offset: -2px;
-        }
-
-        ::-webkit-search-decoration {
-            -webkit-appearance: none;
-        }
-
-        ::-webkit-file-upload-button {
-            -webkit-appearance: button;
-            font: inherit;
         }
 
         summary {
             display: list-item;
         }
 
-        blockquote,
-        dd,
-        dl,
-        figure,
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6,
-        hr,
-        p,
-        pre {
-            margin: 0;
-        }
-
-        fieldset {
-            margin: 0;
-            padding: 0;
-        }
-
-        legend {
-            padding: 0;
-        }
-
-        menu,
-        ol,
-        ul {
-            list-style: none;
-            margin: 0;
-            padding: 0;
-        }
-
-        dialog {
-            padding: 0;
-        }
-
-        textarea {
-            resize: vertical;
-        }
-
-        input::placeholder,
-        textarea::placeholder {
-            opacity: 1;
-            color: #9ca3af;
-        }
-
-        [role=button],
-        button {
-            cursor: pointer;
-        }
-
-        :disabled {
-            cursor: default;
-        }
-
-        audio,
-        canvas,
-        embed,
-        iframe,
-        img,
-        object,
-        svg,
-        video {
+        audio, canvas, embed, iframe, img, object, svg, video {
             display: block;
-            vertical-align: middle;
-        }
-
-        img,
-        video {
             max-width: 100%;
             height: auto;
         }
@@ -245,7 +73,14 @@
 
         /* Custom styles */
         body {
-            font-family: Figtree, ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-            font-feature-settings: normal;
-            font-variation-settings: normal;
-            -webkit-text
+            --tw-text-opacity: 1;
+            color: rgb(31 41 55 / var(--tw-text-opacity));
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Your content goes here -->
+</body>
+
+</html>
