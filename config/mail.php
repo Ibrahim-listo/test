ab41e09090d1c6cf00c140414a6c91b3d78eb213
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:22 +0200

    comment

--- config/mail.php
+++ config/mail.php
@@ -1,23 +1,8 @@
 <?php
 
+// Return an array of mail configuration options.
 return [
     /*
     |--------------------------------------------------------------------------
     | Default Mailer
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default mailer that is used to send all email
-    | messages unless another mailer is explicitly specified when sending
-    | the message. All additional mailers can be configured within the
-    | "mailers" array.
-    |
-    */
-
-    'default' => env('MAIL_MAILER', 'smtp'), // default mailer is smtp
-
-    /*
-    |--------------------------------------------------------------------------
-    | Mailer Configurations
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure all of the mailer's that are provided with
+    |------------------------------------------------
