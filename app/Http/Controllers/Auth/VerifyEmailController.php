fe0f4435149a5ceb90db449efaf98ae595bd5210
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:13 +0200

    improve

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Events\UserEmailVerified;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
@@ -19,12 +20,36 @@ class VerifyEmailController extends Controller
         $user = $request->user();
 
         if ($user->hasVerifiedEmail()) {
-            return Redirect::intended(route('dashboard', ['verified' => 1], false));
+            return redirect()->intended(route('dashboard', ['verified' => 1], false));
         }
 
         $user->markEmailAsVerified();
+        event(new UserEmailVerified($user));
         event(new Verified($user));
 
-        return Redirect::intended(route('dashboard', ['verified' => 1], false));
+        return redirect()->intended(route('dashboard', ['verified' => 1], false));
     }
 }
+
+
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserEmailVerified
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \App\Models
