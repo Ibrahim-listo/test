61fa43cecd2494274db1a5266a8c0eb96e08487d
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:56:40 +0200

    comment

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,13 @@
 <?php
 
-// Define a new array for the list of providers
+// Define a new array to store the list of service providers
 $providers = array(
+    // Include the AppServiceProvider class
     use App\Providers\AppServiceProvider;
+    // Use the fully qualified class name of AppServiceProvider
     App\Providers\AppServiceProvider::class,
 );
 
-// Return the array of providers
+// Return the array of service providers
 return $providers;
 
