ca95f33a580639c9e6581c5b9e3699f0dd4d4ecd
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:18 +0200

    improve

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -22,15 +22,15 @@ class TaskFactory extends Factory
         return [
             'name' => $this->faker->sentence(),
             'description' => $this->faker->realText(),
-            'due_date' => $this->faker->dateTimeBetween('now', '+1 year'),
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 year')->format('Y-m-d H:i:s'),
             'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed']),
             'priority' => $this->faker->randomElement(['low', 'medium', 'high']),
             'image_path' => $this->faker->imageUrl(),
-            'assigned_user_id' => $this->faker->randomElement(User::pluck('id')),
+            'assigned_user_id' => $this->faker->randomElement(User::pluck('id')) ?? 1,
             'created_by' => 1,
             'updated_by' => 1,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ];
     }
 }
