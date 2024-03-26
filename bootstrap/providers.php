63e240cca98a14c3c1e9640bde3d18e0fa07ba1c
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:12 +0200

    improve

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,10 @@
 <?php
 
 // Define a new array for the list of providers
-$providers = [
+$providers = array(
+    use App\Providers\AppServiceProvider;
     App\Providers\AppServiceProvider::class,
-];
+);
 
 // Return the array of providers
 return $providers;
