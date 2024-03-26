77c4ca76c1999b7b4c73da96292e673f76efef08
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:32 +0200

    improve

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -10,9 +10,29 @@ export default function Checkbox({
   ...props
 }) {
   return (
-    <label htmlFor={id}>
+    <label htmlFor={id} className="flex items-center">
       <input
         {...props}
         type="checkbox"
-        className={
-          'rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-60
+        className={`rounded ${className} dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600`}
+        checked={checked}
+        name={name}
+        tabIndex={tabIndex}
+        onChange={onChange}
+      />
+      <span className="ml-2 text-sm text-gray-600">
+        {props.children}
+      </span>
+    </label>
+  );
+}
+
+Checkbox.propTypes = {
+  className: PropTypes.string,
+  checked: PropTypes.bool,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  tabIndex: PropTypes.number,
+  onChange: PropTypes.func,
+  children: PropTypes.node
+};
