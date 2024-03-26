ffe1d8299364ef0d6cafb273c944229f21b23e53
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:50 +0200

    improve

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,7 +1,16 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
-    return (
-        <label {...props} className={`block font-medium text-sm text-gray-700 dark:text-gray-300 ` + className}>
-            {value ? value : children}
-        </label>
-    );
+import PropTypes from 'prop-types';
+
+export default function StyledInputLabel({ value = '', className, children, htmlFor, ...props }) {
+  return (
+    <label htmlFor={htmlFor} {...props} className={`block font-medium text-sm text-gray-700 dark:text-gray-300 ${className}`}>
+      {value || children}
+    </label>
+  );
 }
+
+StyledInputLabel.propTypes = {
+  value: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  htmlFor: PropTypes.string.isRequired,
+};
