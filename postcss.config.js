58626289aa8f723a446ce9509d099a968668d762
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:44 +0200

    improve

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,31 @@
-// main.js
+// main.config.js
+
+const { TailwindCss } = require('tailwindcss-react-native');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    new TailwindCss({
+      config: './tailwind.config.js',
+    }),
+    autoprefixer,
   ],
 };
 
+
+npm install tailwindcss-react-native
+npm install autoprefixer
+
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
