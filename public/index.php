a8f3081c43222ed9dba4ba37bb073a6550f9b07b
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:58 +0200

    improve

--- public/index.php
+++ public/index.php
@@ -1,17 +1,27 @@
 <?php
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+// Define the start time of the application
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
+    http_response_code(Response::HTTP_SERVICE_UNAVAILABLE);
+    echo 'The application is currently in maintenance mode. Please try again later.';
+    exit;
 }
 
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$response = $app->handleRequest(Request::capture());
+
+// Send the response and end the request
+$response->send();
+$app->terminate();
