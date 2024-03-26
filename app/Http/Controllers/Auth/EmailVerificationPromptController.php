b4e2951e71a056f823914f53f7ff9f5127d9fe72
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:14 +0200

    improve

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,8 +15,14 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|Response
     {
-        return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
-                    : Inertia::render('Auth/VerifyEmail', ['status' => session('status')]);
+        $user = $request->user();
+
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->intended(route('dashboard', absolute: false));
+        }
+
+        return Inertia::render('Auth/VerifyEmail', [
+            'status' => $request->session()->get('status'),
+        ]);
     }
 }
