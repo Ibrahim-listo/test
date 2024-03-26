347e673757801ae3d527c9a5bc3fc2bc9522dbb4
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:17 +0200

    improve

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index()
+    {
+        return 'This is the index method of the abstract controller class';
+    }
 }
