ee55811d9e3deef6cfe5d36a863fd657ba0db3a8
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:51 +0200

    improve

--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -1,7 +1,18 @@
-export default function InputError({ message, className = '', ...props }) {
-    return message ? (
-        <p {...props} className={'text-sm text-red-600 dark:text-red-400 ' + className}>
-            {message}
-        </p>
-    ) : null;
+import PropTypes from 'prop-types';
+
+export default function InputError({ message, className, ...props }) {
+  const errorClasses = 'text-sm text-red-600 dark:text-red-400';
+
+  // Return null if there's no error message
+  if (!message) {
+    return null;
+  }
+
+  // Spread the props object before the errorClasses to ensure it's not overwritten
+  return <p {...props} className={`${errorClasses} ${className}`}>{message}</p>;
 }
+
+InputError.propTypes = {
+  message: PropTypes.string,
+  className: PropTypes.string,
+};
