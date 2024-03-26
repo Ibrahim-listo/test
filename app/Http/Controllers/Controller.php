ec2c9390d8997107256e9fe267af722b0d06437f
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:55:42 +0200

    improve

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+abstract class BaseController
 {
+    /**
+     * Display the index route.
+     *
+     * @return string
+     */
     public function index()
     {
         return 'This is the index method of the abstract controller class';
