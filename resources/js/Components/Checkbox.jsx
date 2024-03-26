34cd58a7473b6b21f94552ceca9f530e049a6da7
Ibrahim-listo <165072351+Ibrahim-listo@users.noreply.github.com>
2024-03-26 20:54:23 +0200

    improve

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -1,12 +1,18 @@
-export default function Checkbox({ className = '', ...props }) {
-    return (
-        <input
-            {...props}
-            type="checkbox"
-            className={
-                'rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 ' +
-                className
-            }
-        />
-    );
-}
+import PropTypes from 'prop-types';
+
+export default function Checkbox({
+  className = '',
+  checked = false,
+  name = '',
+  id = '',
+  tabIndex = 0,
+  onChange = () => {},
+  ...props
+}) {
+  return (
+    <label htmlFor={id}>
+      <input
+        {...props}
+        type="checkbox"
+        className={
+          'rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-60
